9fec0f509a01752eaec7f5109df362498f6e3591
asmarcd <asmarcd@gmail.com>
2021-03-09 15:42:08 -0800

    working connection to back end via useeffect on start page

--- src/components/TicketDisplay.js
+++ src/components/TicketDisplay.js
@@ -1,18 +1,24 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import API from '../utils/API';
 
 const TicketDisplay = () => {
 
-    const [ticketView, setTicketView] = useState([]);
+    const [tickets, setTickets] = useState([]);
 
     useEffect(() => {
-        // reference api to pull in relevant tickets.
-    })
+        API.allTickets()
+            .then(result => {
+                setTickets(result)
+            })
+    }, [])
 
 
     return (
         <div>
             <h1>Tickets will go here</h1>
-            <p>Here's a ticket</p>
+            <div className='ticketContainer'>
+
+            </div>
         </div>
     )
 }
---
+++ src/utils/API.js
@@ -0,0 +1,37 @@
+// for dev purposes only:
+const URL_PREFIX = 'http://localhost:3001';
+
+const API = {
+
+    // see all tickets
+    allTickets: async (tickets) => {
+        try {
+            const res = await fetch(`${URL_PREFIX}/tickets`, {
+                method: 'GET',
+                body: JSON.stringify(tickets)
+            });
+            return await res.json();
+        } catch (err) {
+            return null;
+        }
+    },
+
+    // create a new ticket
+    createTicket: async newTicket => {
+        try {
+            const res = await fetch(`${URL_PREFIX}/tickets`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(newTicket)
+            });
+            return await res.json();
+        } catch (err) {
+            return null;
+        }
+    }
+    // remember to add comma above when you add next function
+}
+
+module.exports = API;
