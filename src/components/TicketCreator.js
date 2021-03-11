7fd70b78ad686e9a4b29c46e6c3967a765b089f6
asmarcd <asmarcd@gmail.com>
2021-03-11 11:55:52 -0800

    form outline for ticket submission

--- src/components/TicketCreator.js
+++ src/components/TicketCreator.js
@@ -5,11 +5,46 @@ const TicketCreator = () => {
 
     const toggleTicketCreator = useContext(ViewContext);
 
+    const submitTicket = () => {
+
+
+        toggleTicketCreator();
+    };
+
+    const cancelTicket = () => {
+
+
+
+        toggleTicketCreator();
+    }
+
     return (
         <div>
-            <h1>Ticket Creator</h1>
-            <button onClick={toggleTicketCreator}>Submit</button>
-            <button onClick={toggleTicketCreator}>Cancel</button>
+            <h1>Submit a New Ticket</h1>
+            <form>
+                <label>
+                    Title:
+                    <input type="text" name="name" />
+                </label>
+                <br />
+                <label>
+                    Label:
+                    <input type="text" name="label" />
+                </label>
+                <br />
+                <label>
+                    Description:
+                    <input type="text" name="description" />
+                </label>
+                <br />
+                <label>
+                    Priority:
+                    <input type="range" name="priority" />
+                </label>
+                <br />
+                <button type='submit' onClick={submitTicket}>Submit</button>
+                <button onClick={cancelTicket}>Cancel</button>
+            </form>
         </div>
     )
 }
