0688a8cdadcb8c227612616dd572e9090ea6b8de
asmarcd <asmarcd@gmail.com>
2021-03-18 10:10:10 -0700

    split Ticket component from TicketDisplay. Delete button working. Edit still needs to be hooked up.

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import API from '../utils/API';
+import { UpdatePageContext } from './TicketDisplay';
 
-const Ticket = () => {
+const Ticket = props => {
+
+    const updatePage = useContext(UpdatePageContext);
+
+    const editTicket = e => {
+        console.log('testing');
+    };
+
+    const deleteTicket = e => {
+        API.deleteTicket(e.target.value)
+            .then(updatePage())
+    };
 
     return (
-        <div>
-            <p>Ticket Layout Placeholder</p>
+        <div key={props.props.id}>
+            <h3>{props.props.title}</h3>
+            <h4>Creator: TBD Owner: {props.props.owner}</h4>
+            <h4>Created: {props.props.createdAt} Last Updated: {props.props.updatedAt}</h4>
+            <p>{props.props.label}</p>
+            <p>{props.props.status}</p>
+            <p>{props.props.description}</p>
+            <p>Priority {props.props.priority}</p>
+            <button value={props.props.id} onClick={editTicket}>Edit</button>
+            <button value={props.props.id} onClick={deleteTicket}>Delete</button>
         </div>
     )
 }
--- src/components/TicketDisplay.js
+++ src/components/TicketDisplay.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import API from '../utils/API';
-import useForm from '../utils/CustomHooks';
+import Ticket from './Ticket';
+
+export const UpdatePageContext = React.createContext();
 
 const TicketDisplay = () => {
 
@@ -11,14 +13,9 @@ const TicketDisplay = () => {
     useEffect(() => {
         API.allTickets()
             .then(result => {
-                setTickets(result)
+                setTickets(result);
             })
     }, [updatePage]);
-
-    const deleteTicket = e => {
-        API.deleteTicket(e.target.value)
-            .then(ticketViewRefresh())
-    }
 
     const ticketViewRefresh = () => {
         if (!updatePage) {
@@ -26,21 +23,11 @@ const TicketDisplay = () => {
         } else {
             setUpdatePage(false);
         }
-    }
+    };
 
     const toggleArchived = () => {
         showArchived ? setShowArchived(false) : setShowArchived(true);
-    }
-
-    const editTicket = e => {
-        // click event is set up to properly reference the id of the ticket, but the actual interface to do the editing doesn't exist yet. Two possible approaches. 1. get Ticket Creator to reopen with the relevant inputs in place, then submit that once edited to change the ticket. 2. Create another editor component that looks similar, edits the ticket instead. I think option two, despite requiring a separate component, will be easier to create and customize.
-        console.log(e.target.value)
-
-        // API.editTicket(e.target.value)
-        // .then(ticketViewRefresh());
-    }
-
-    const { inputs, handleInputChange, handleSubmit } = useForm(editTicket);
+    };
 
 
     return (
@@ -52,19 +39,12 @@ const TicketDisplay = () => {
                     {tickets.map(ticket => {
                         if (ticket.status === 'To Do') {
                             return (
-                                <div key={ticket.id}>
-                                    <h3>{ticket.title}</h3>
-                                    <h4>Creator: TBD Owner: {ticket.owner}</h4>
-                                    <h4>Created: {ticket.createdAt} Last Updated: {ticket.updatedAt}</h4>
-                                    <p>{ticket.label}</p>
-                                    <p>{ticket.status}</p>
-                                    <p>{ticket.description}</p>
-                                    <p>Priority {ticket.priority}</p>
-                                    <button value={ticket.id} onClick={editTicket}>Edit</button>
-                                    <button value={ticket.id} onClick={deleteTicket}>Delete</button>
-                                </div>
+                                <UpdatePageContext.Provider value={ticketViewRefresh}>
+                                    <Ticket props={ticket} />
+                                </UpdatePageContext.Provider>
                             )
                         }
+                        return ("")
                     })}
                 </div>
                 <h2>In Progress</h2>
@@ -72,19 +52,12 @@ const TicketDisplay = () => {
                     {tickets.map(ticket => {
                         if (ticket.status === 'In Progress') {
                             return (
-                                <div key={ticket.id}>
-                                    <h3>{ticket.title}</h3>
-                                    <h4>Creator: TBD Owner: {ticket.owner}</h4>
-                                    <h4>Created: {ticket.createdAt} Last Updated: {ticket.updatedAt}</h4>
-                                    <p>{ticket.label}</p>
-                                    <p>{ticket.status}</p>
-                                    <p>{ticket.description}</p>
-                                    <p>Priority {ticket.priority}</p>
-                                    <button value={ticket.id} onClick={editTicket}>Edit</button>
-                                    <button value={ticket.id} onClick={deleteTicket}>Delete</button>
-                                </div>
+                                <UpdatePageContext.Provider value={ticketViewRefresh}>
+                                    <Ticket props={ticket} />
+                                </UpdatePageContext.Provider>
                             )
                         }
+                        return ("")
                     })}
                 </div>
                 <h2>Complete</h2>
@@ -92,44 +65,30 @@ const TicketDisplay = () => {
                     {tickets.map(ticket => {
                         if (ticket.status === 'Complete') {
                             return (
-                                <div key={ticket.id}>
-                                    <h3>{ticket.title}</h3>
-                                    <h4>Creator: TBD Owner: {ticket.owner}</h4>
-                                    <h4>Created: {ticket.createdAt} Last Updated: {ticket.updatedAt}</h4>
-                                    <p>{ticket.label}</p>
-                                    <p>{ticket.status}</p>
-                                    <p>{ticket.description}</p>
-                                    <p>Priority {ticket.priority}</p>
-                                    <button value={ticket.id} onClick={editTicket}>Edit</button>
-                                    <button value={ticket.id} onClick={deleteTicket}>Delete</button>
-                                </div>
+                                <UpdatePageContext.Provider value={ticketViewRefresh}>
+                                    <Ticket props={ticket} />
+                                </UpdatePageContext.Provider>
                             )
                         }
+                        return ("")
                     })}
                 </div>
                 {!showArchived ? <button onClick={toggleArchived}>Show Archived Tickets</button> :
                     <div>
                         <h2>Archived</h2>
-                        <button onClick={toggleArchived}>Hide Archived Tickets</button>
                         <div className='archivedCol'>
                             {tickets.map(ticket => {
                                 if (ticket.status === 'Archived') {
                                     return (
-                                        <div key={ticket.id}>
-                                            <h3>{ticket.title}</h3>
-                                            <h4>Creator: TBD Owner: {ticket.owner}</h4>
-                                            <h4>Created: {ticket.createdAt} Last Updated: {ticket.updatedAt}</h4>
-                                            <p>{ticket.label}</p>
-                                            <p>{ticket.status}</p>
-                                            <p>{ticket.description}</p>
-                                            <p>Priority {ticket.priority}</p>
-                                            <button value={ticket.id} onClick={editTicket}>Edit</button>
-                                            <button value={ticket.id} onClick={deleteTicket}>Delete</button>
-                                        </div>
+                                        <UpdatePageContext.Provider value={ticketViewRefresh}>
+                                            <Ticket props={ticket} />
+                                        </UpdatePageContext.Provider>
                                     )
                                 }
+                                return ("")
                             })}
                         </div>
+                        <button onClick={toggleArchived}>Hide Archived Tickets</button>
                     </div>
                 }
             </section>
